7e4c42df084dedb6d57036dbbf4ff79013a3c111
Lui <69409903+LuiArroyo@users.noreply.github.com>
2023-10-28 19:01:18 -0500

    Update NavBar.vue

--- OpenGlass_1.0/src/components/NavBar.vue
+++ OpenGlass_1.0/src/components/NavBar.vue
@@ -58,6 +58,7 @@ const mobileMenuOpen = ref(false)
             <div class="-my-6 divide-y divide-gray-500/10">
               <div class="space-y-2 py-6">
                 <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+                <a href="/orders" v-if="isAuthenticated" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Orders</a>
               </div>
               <div class="py-6">
                 <a href="/login" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
