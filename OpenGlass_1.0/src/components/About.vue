2c94790392f0e71939b523d01d7e02df1fea2390
LuiArroyo <arroyo0099@yahoo.com>
2023-11-06 14:08:15 -0600

    Added About Us content

--- OpenGlass_1.0/src/components/About.vue
+++ OpenGlass_1.0/src/components/About.vue
@@ -1,35 +1,42 @@
 <template>
 <!-- Content -->
-    <div class="container mx-auto mt-8 p-20">
+    <div class="container mx-auto mt-8 p-16">
         <div class="flex flex-col md:flex-row items-center">
         <!-- Image with circular radius -->
-            <div class="w-full md:w-1/2 md:pr-20">
+            <div class="w-full md:w-1/2">
                 <div class="rounded-full h-45 w-45 md:h-70 md:w-70 bg-gray-300 overflow-hidden flex items-center justify-center md:ml-12">
-                    <img src="/gal_img2.png" alt="About Us" class="rounded-full w-full h-auto flex-shrink-0">
+                    <img src="/gal_img2.png" alt="About Us" class="rounded-full w-full h-auto flex-shrink-0" style="min-width: 100%; min-height: 100%; max-width: 100%; max-height: auto;">
                 </div>
             </div>
             <!-- Short paragraph on the right of the image -->
-            <div class="w-full mt-4 md:mt-0 text-center md:text-left md:mr-12">
+            <div class="w-full mt-8 md:mt-0 text-center md:text-left md:mr-12 md:ml-8">
                 <h2 class="text-2xl font-bold mb-4">We are OpenGlass</h2>
                 <p>
-                    Marlene and Carlos, two inspired individuals, discovered their passion for creating Latino-inspired drinks
-                    during gatherings with close friends and loved ones on special occasions. Their journey didn't start in a
-                    b        ar, but in the heartwarming moments of these gatherings. Amidst laughter and shared stories, they
-found joy in crafting drinks that celebrated their heritage and the lively Latino culture.
+                Marlene and Carlos, two inspired individuals, discovered their passion for creating Latino-inspired drinks
+                during gatherings with close friends and loved ones on special occasions. Their journey didn't start in a
+                bar, but in the heartwarming moments of these gatherings. Amidst laughter and shared stories, they
+                found joy in crafting drinks that celebrated their heritage and the lively Latino culture.
                 <br><br>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a odio in libero sagittis aliquam non ac odio.
-                Nulla facilisi. Donec consequat tristique eros, nec semper purus vehicula at. Vestibulum non mattis libero.
+                As a couple, their journey wasn't solely about mixing drinks; it was a love story with the art of mixology
+                and a commitment to bringing people together. Marlene and Carlos put their hearts into every cocktail
+                they created. For them, it was about establishing a place where each drink had a story to tell, and every
+                sip was a journey into the rich world of Latin taste and authenticity. 
                 <br><br>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a odio in libero sagittis aliquam non ac odio.
-                Nulla facilisi. Donec consequat tristique eros, nec semper purus vehicula at. Vestibulum non mattis libero.
+                Together, they embarked on a shared mission to create "Open Glass," a mobile bar like no other. It's not
+                just about serving drinks; it's about crafting unforgettable moments. They are dedicated to delivering
+                top-notch quality, infusing a creative touch, and adding a hint of Latin flavors that connect you to the
+                heart and soul of their culture.
                 </p>
             </div>
         </div>
     </div>
       <!-- Instagram button section -->
-      <div class="p-16">
-        <a href="https://www.instagram.com/yourinstagram" target="_blank" class="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-500 hover:text-white transition">
+      <div class="p-8">
+        <a href="https://www.instagram.com/openglassmobilebar" target="_blank" class="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-500 hover:text-white transition">
           Follow us on Instagram
         </a>
       </div>
 </template>
+
+<style scoped>
+</style>
