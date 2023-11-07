e4880f56024ef8650710fec31e9168428e248700
cjnormandy <cjnorman@cougarnet.uh.edu>
2023-11-06 21:49:43 -0600

    PDF changes and etc..

--- OpenGlass_1.0/src/components/PackagesView/SignatureInput.vue
+++ OpenGlass_1.0/src/components/PackagesView/SignatureInput.vue
@@ -2,12 +2,13 @@
     <div>
       <!-- Display Mode -->
 
-      <div v-if="!isEditing" @click="isEditing = true" class="mt-1 p-2 rounded-md shadow-sm border bg-white cursor-text placeholder-gray-400">
+      <!-- <div v-if="!isEditing" @click="isEditing = true" class="mt-1 p-2 rounded-md shadow-sm border bg-white cursor-text placeholder-gray-400">
         <vue-pdf-embed :source="'/service_agreement.pdf'" />
         <p v-if="signatureName" class="font-dancingScript text-xl">{{ signatureName }}</p>
         <p v-else class="text-gray-400">Click to sign</p>
-      </div>
+      </div> -->
   
+      <customer-form/>
       <!-- Editing Mode -->
       <input v-if="isEditing" type="text" :value="signatureName" @blur="isEditing = false" class="mt-1 p-2 rounded-md shadow-sm bg-white" placeholder="Type your name here" ref="signatureInput" />
     </div>
@@ -15,13 +16,15 @@
   
   <script>
   import VuePdfEmbed from 'vue-pdf-embed'
+  import CustomerForm from './CustomerForm.vue';
   export default {
     props: {
       signatureName: String,
     },
     components: {
-      VuePdfEmbed,
-    },
+    VuePdfEmbed,
+    CustomerForm
+},
     data() {
       return {
         source1: 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf',
